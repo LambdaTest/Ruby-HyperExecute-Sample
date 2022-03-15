388623bdf76b729a554d276e2f1383389380b92b
Lambda Test <lambdatest@Lambdas-MacBook-Pro.local>
2022-03-15 22:17:58 +0530

    added code

---
+++ scripts/lambdatest.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+require 'rspec'
+require 'selenium-webdriver'
+
+CONFIG_NAME = ENV['CONFIG_NAME'] || 'single'
+
+CONFIG = YAML.load(File.read(File.join(File.dirname(__FILE__), "../config/#{CONFIG_NAME}.config.yml")))
+CONFIG['user'] = ENV['LT_USERNAME'] || CONFIG['user']
+CONFIG['key'] = ENV['LT_ACCESS_KEY'] || CONFIG['key']
+
+RSpec.configure do |config|
+  config.around(:example) do |example|
+    @caps = CONFIG['common_caps'].merge(CONFIG['browser_caps'][0])
+    @caps["name"] = ENV['name'] || example.metadata[:name] || example.metadata[:file_path].split('/').last.split('.').first
+
+    @driver = Selenium::WebDriver.for(:remote,:url => "https://#{CONFIG['user']}:#{CONFIG['key']}@#{CONFIG['server']}/wd/hub",:capabilities => @caps)
+
+    begin
+      example.run
+    ensure
+      @driver.quit
+    end
+  end
+end
