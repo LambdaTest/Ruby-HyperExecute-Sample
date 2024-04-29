cba8b372a36c62edaec5e91f57f4aa65a82ce13f
amanchopra1905 <amanc@lambdatest.com>
2024-04-29 23:19:17 +0530

    updated the script file

--- scripts/lambdatest.rb
+++ scripts/lambdatest.rb
@@ -10,15 +10,26 @@ CONFIG['key'] = ENV['LT_ACCESS_KEY'] || CONFIG['key']
 
 RSpec.configure do |config|
   config.around(:example) do |example|
-    @caps = CONFIG['common_caps'].merge(CONFIG['browser_caps'][0])
+    @caps = Selenium::WebDriver::Remote::Capabilities.new(CONFIG['common_caps'].merge(CONFIG['browser_caps'][0]))
     @caps["name"] = ENV['name'] || example.metadata[:name] || example.metadata[:file_path].split('/').last.split('.').first
 
-    @driver = Selenium::WebDriver.for(:remote,:url => "https://#{CONFIG['user']}:#{CONFIG['key']}@#{CONFIG['server']}/wd/hub",:capabilities => @caps)
-    @wait = Selenium::WebDriver::Wait.new(:timeout => 15)
+    @driver = Selenium::WebDriver.for(:remote, url: "https://#{CONFIG['user']}:#{CONFIG['key']}@#{CONFIG['server']}/wd/hub", desired_capabilities: @caps)
+    @wait = Selenium::WebDriver::Wait.new(timeout: 15)
     begin
       example.run
     ensure
-      @driver.quit
+      retry_count = 1
+      begin
+        @driver.quit
+      rescue Selenium::WebDriver::Error::WebDriverError => e
+        retry_count -= 1
+        if retry_count.zero?
+          # puts "Failed to quit WebDriver session: #{e.message}"
+        else
+          sleep 1
+          retry
+        end
+      end
     end
   end
 end
